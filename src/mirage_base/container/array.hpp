bd4c70c937fd03933c9573797538e82609d2b714
wangzekai <wangzekai.parallel@bytedance.com>
2024-06-06 21:05:34 +0800

    Add Array.

---
+++ src/mirage_base/container/array.hpp
@@ -0,0 +1,382 @@
+#ifndef MIRAGE_BASE_CONTAINER_ARRAY
+#define MIRAGE_BASE_CONTAINER_ARRAY
+
+#include <concepts>
+#include <initializer_list>
+#include <iterator>
+
+#include "mirage_base/define.hpp"
+#include "mirage_base/util/aligned_memory.hpp"
+
+namespace mirage {
+
+template <std::move_constructible T>
+class ArrayConstIterator;
+
+template <std::move_constructible T>
+class ArrayIterator {
+ public:
+  using iterator_concept = std::contiguous_iterator_tag;
+  using iterator_category = std::random_access_iterator_tag;
+  using iterator_type = ArrayIterator;
+  using difference_type = ptrdiff_t;
+  using value_type = T;
+  using pointer = value_type*;
+  using reference = value_type&;
+
+  ArrayIterator() = default;
+  ~ArrayIterator() = default;
+
+  ArrayIterator(const ArrayIterator& other) : ptr_(other.ptr_) {}
+
+  explicit ArrayIterator(pointer ptr) : ptr_(ptr) {}
+
+  reference operator*() const { return *ptr_; }
+
+  pointer operator->() const { return ptr_; }
+
+  reference operator[](difference_type diff) const { return ptr_[diff]; }
+
+  iterator_type& operator++() {
+    if (ptr_ != nullptr) {
+      ++ptr_;
+    }
+    return *this;
+  }
+
+  iterator_type operator++(int) {
+    iterator_type temp(*this);
+    ++(*this);
+    return temp;
+  }
+
+  iterator_type& operator--() {
+    --ptr_;
+    return *this;
+  }
+
+  iterator_type operator--(int) {
+    iterator_type temp(*this);
+    --(*this);
+    return temp;
+  }
+
+  iterator_type& operator+=(difference_type diff) {
+    ptr_ += diff;
+    return *this;
+  }
+
+  iterator_type operator+(difference_type diff) const {
+    iterator_type temp(*this);
+    temp += diff;
+    return temp;
+  }
+
+  iterator_type& operator-=(difference_type diff) {
+    ptr_ -= diff;
+    return *this;
+  }
+
+  iterator_type operator-(difference_type diff) const {
+    iterator_type temp(*this);
+    temp -= diff;
+    return temp;
+  }
+
+  difference_type operator-(const iterator_type& other) const {
+    return ptr_ - other.ptr_;
+  }
+
+  bool operator==(const iterator_type& other) const {
+    return ptr_ == other.ptr_;
+  }
+
+  bool operator<(const iterator_type& other) const { return ptr_ < other.ptr_; }
+
+  bool operator>(const iterator_type& other) const { return other < *this; }
+
+  bool operator>=(const iterator_type& other) const { return !(*this < other); }
+
+  bool operator<=(const iterator_type& other) const { return !(other < *this); }
+
+ private:
+  friend class ArrayConstIterator<T>;
+
+  pointer ptr_{nullptr};
+};
+
+template <std::move_constructible T>
+ArrayIterator<T> operator+(typename ArrayIterator<T>::difference_type diff,
+                           ArrayIterator<T> iter) {
+  return iter + diff;
+}
+
+template <std::move_constructible T>
+class ArrayConstIterator {
+ public:
+  using iterator_concept = std::contiguous_iterator_tag;
+  using iterator_category = std::random_access_iterator_tag;
+  using iterator_type = ArrayConstIterator;
+  using difference_type = ptrdiff_t;
+  using value_type = const T;
+  using pointer = value_type*;
+  using reference = value_type&;
+
+  ArrayConstIterator() = default;
+  ~ArrayConstIterator() = default;
+
+  ArrayConstIterator(const ArrayConstIterator& other) : ptr_(other.ptr_) {}
+
+  explicit ArrayConstIterator(const ArrayIterator<T>& iter) : ptr_(iter.ptr_) {}
+
+  explicit ArrayConstIterator(pointer ptr) : ptr_(ptr) {}
+
+  reference operator*() const { return *ptr_; }
+
+  pointer operator->() const { return ptr_; }
+
+  reference operator[](difference_type diff) const { return ptr_[diff]; }
+
+  iterator_type& operator++() {
+    if (ptr_ != nullptr) {
+      ++ptr_;
+    }
+    return *this;
+  }
+
+  iterator_type operator++(int) {
+    iterator_type temp(*this);
+    ++(*this);
+    return temp;
+  }
+
+  iterator_type& operator--() {
+    --ptr_;
+    return *this;
+  }
+
+  iterator_type operator--(int) {
+    iterator_type temp(*this);
+    --(*this);
+    return temp;
+  }
+
+  iterator_type& operator+=(difference_type diff) {
+    ptr_ += diff;
+    return *this;
+  }
+
+  iterator_type operator+(difference_type diff) const {
+    iterator_type temp(*this);
+    temp += diff;
+    return temp;
+  }
+
+  iterator_type& operator-=(difference_type diff) {
+    ptr_ -= diff;
+    return *this;
+  }
+
+  iterator_type operator-(difference_type diff) const {
+    iterator_type temp(*this);
+    temp -= diff;
+    return temp;
+  }
+
+  difference_type operator-(const iterator_type& other) const {
+    return ptr_ - other.ptr_;
+  }
+
+  bool operator==(const iterator_type& other) const {
+    return ptr_ == other.ptr_;
+  }
+
+  bool operator<(const iterator_type& other) const { return ptr_ < other.ptr_; }
+
+  bool operator>(const iterator_type& other) const { return other < *this; }
+
+  bool operator>=(const iterator_type& other) const { return !(*this < other); }
+
+  bool operator<=(const iterator_type& other) const { return !(other < *this); }
+
+ private:
+  pointer ptr_{nullptr};
+};
+
+template <std::move_constructible T>
+ArrayConstIterator<T> operator+(
+    typename ArrayConstIterator<T>::difference_type diff,
+    ArrayConstIterator<T> iter) {
+  return iter + diff;
+}
+
+template <std::move_constructible T>
+class Array {
+ public:
+  using Iterator = ArrayIterator<T>;
+  using ConstIterator = ArrayConstIterator<T>;
+
+  Array() = default;
+
+  Array(const Array& other) {
+    if constexpr (!std::copy_constructible<T>) {
+      MIRAGE_DCHECK(false);  // This type is supposed to be copyable.
+    } else {
+      Reserve(other.size_);
+      for (const T& val : other) {
+        Push(val);
+      }
+    }
+  }
+
+  Array(Array&& other) noexcept
+      : data_(other.data_), size_(other.size_), capacity_(other.capacity_) {
+    other.size_ = 0;
+    other.capacity_ = 0;
+    other.data_ = nullptr;
+  }
+
+  Array(std::initializer_list<T> list) {
+    Reserve(list.size());
+    for (const T& val : list) {
+      Push(val);
+    }
+  }
+
+  ~Array() noexcept { Clear(); }
+
+  void Clear() {
+    for (size_t i = 0; i < size_; ++i) {
+      data_[i].GetPtr()->~T();
+    }
+    delete[] data_;
+    data_ = nullptr;
+    size_ = 0;
+    capacity_ = 0;
+  }
+
+  void Push(const T& val) {
+    if constexpr (!std::copy_constructible<T>) {
+      MIRAGE_DCHECK(false);  // This type is supposed to be copyable.
+    } else {
+      Emplace(T(val));
+    }
+  }
+
+  template <typename... Args>
+  void Emplace(Args&&... args) {
+    EnsureNotFull();
+    new (data_[size_].GetPtr()) T(std::forward<Args>(args)...);
+    ++size_;
+  }
+
+  T Pop() {
+    MIRAGE_DCHECK(size_ != 0);
+    --size_;
+    return std::move(data_[size_].GetRef());
+  }
+
+  T& operator[](size_t index) { return data_[index].GetRef(); }
+
+  bool operator==(const Array& other) const {
+    if (size_ != other.size_) {
+      return false;
+    }
+    if (data_ == other.data_) {
+      return true;
+    }
+    if constexpr (!std::equality_comparable<T>) {
+      return false;  // Can't be compared.
+    } else {
+      for (size_t i = 0; i < size_; ++i) {
+        if (data_[i].GetRef() != other.data_[i].GetRef()) {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+
+  void Reserve(size_t capacity) {
+    if (capacity <= capacity_) {
+      return;
+    }
+    SetCapacity(capacity);
+  }
+
+  T* GetRawPtr() const { return (T*)data_; }
+
+  [[nodiscard]] size_t GetSize() const { return size_; }
+
+  void SetSize(size_t size) {
+    if (size == size_) {
+      return;
+    } else if (size < size_) {
+      while (size < size_) {
+        --size_;
+        data_[size_].GetPtr()->~T();
+      }
+      return;
+    }
+
+    if constexpr (!std::default_initializable<T>) {
+      MIRAGE_DCHECK(false);
+    } else {
+      Reserve(size);
+      while (size > size_) {
+        new (data_[size_].GetPtr()) T();
+        ++size_;
+      }
+    }
+  }
+
+  [[nodiscard]] bool IsEmpty() const { return size_ == 0; }
+
+  [[nodiscard]] size_t GetCapacity() const { return capacity_; }
+
+  void SetCapacity(size_t capacity) {
+    if (capacity == capacity_) {
+      return;
+    }
+
+    auto* data = new AlignedMemory<T>[capacity]();
+    size_t size = capacity < size_ ? capacity : size_;
+    for (size_t i = 0; i < size; ++i) {
+      T* ptr = data_[i].GetPtr();
+      new (data[i].GetPtr()) T(std::move(*ptr));
+      ptr->~T();
+    }
+    delete[] data_;
+
+    data_ = data;
+    size_ = size;
+    capacity_ = capacity;
+  }
+
+  Iterator begin() { return Iterator(GetRawPtr()); }
+
+  Iterator end() { return Iterator(GetRawPtr() + size_); }
+
+  ConstIterator begin() const { return ConstIterator(GetRawPtr()); }
+
+  ConstIterator end() const { return ConstIterator(GetRawPtr() + size_); }
+
+ private:
+  void EnsureNotFull() {
+    if (capacity_ == 0) {
+      capacity_ = 1;
+      data_ = new AlignedMemory<T>[1]();
+    } else if (size_ == capacity_) {
+      SetCapacity(2 * capacity_);
+    }
+  }
+
+  AlignedMemory<T>* data_{nullptr};
+  size_t size_{0};
+  size_t capacity_{0};
+};
+
+}  // namespace mirage
+
+#endif  // MIRAGE_BASE_CONTAINER_ARRAY
---
+++ test/mirage_base/array_tests.cpp
@@ -0,0 +1,89 @@
+#include <gtest/gtest.h>
+
+#include "mirage_base/auto_ptr/owned.hpp"
+#include "mirage_base/container/array.hpp"
+
+using namespace mirage;
+
+TEST(ArrayTests, Construct) {
+  Array<int32_t> array = {0, 1, 2};
+
+  Array<int32_t> copy_array(array);
+  EXPECT_EQ(array, copy_array);
+
+  int32_t* raw_ptr = array.GetRawPtr();
+  Array<int32_t> move_array(std::move(array));
+  EXPECT_TRUE(array.IsEmpty());  // NOLINT(*-use-after-move): Allow for test.
+  EXPECT_EQ(array.GetRawPtr(), nullptr);
+  EXPECT_EQ(raw_ptr, move_array.GetRawPtr());
+  EXPECT_EQ(move_array, copy_array);
+}
+
+TEST(ArrayTests, DestructAfterMoved) {
+  int32_t destruct_cnt = 0;
+  auto destructor = [](int32_t* ptr) {
+    *ptr += 1;
+  };
+
+  {
+    Array<Owned<int32_t>> src;
+    src.Emplace(Owned<int32_t>(&destruct_cnt, destructor));
+    src.Emplace(Owned<int32_t>(&destruct_cnt, destructor));
+    Array<Owned<int32_t>> dst(std::move(src));
+    Owned<int32_t> pop = dst.Pop();
+    EXPECT_EQ(destruct_cnt, 0);
+  }
+  EXPECT_EQ(destruct_cnt, 2);
+}
+
+TEST(ArrayTests, ChangeSizeAndCapacity) {
+  Array<int32_t> array = {0, 1, 2};
+  EXPECT_EQ(array.GetSize(), 3);
+  EXPECT_EQ(array.GetCapacity(), 3);
+
+  array.Push(3);
+  EXPECT_EQ(array.GetSize(), 4);
+  EXPECT_EQ(array.GetCapacity(), 6);
+
+  array.Reserve(5);
+  EXPECT_EQ(array.GetSize(), 4);
+  EXPECT_EQ(array.GetCapacity(), 6);
+
+  array.SetCapacity(5);
+  EXPECT_EQ(array.GetSize(), 4);
+  EXPECT_EQ(array.GetCapacity(), 5);
+
+  array.SetSize(5);
+  EXPECT_EQ(array.GetSize(), 5);
+  EXPECT_EQ(array[4], 0);
+  EXPECT_EQ(array.GetCapacity(), 5);
+
+  array.SetSize(3);
+  EXPECT_EQ(array.GetSize(), 3);
+  EXPECT_EQ(array.GetCapacity(), 5);
+}
+
+TEST(ArrayTests, CompareEquality) {
+  Array<int32_t> array_a = {0, 1, 2};
+  Array<int32_t> array_b = {2, 1, 0};
+  EXPECT_EQ(array_a, array_a);
+  EXPECT_NE(array_a, array_b);
+}
+
+TEST(ArrayTests, IterateArray) {
+  EXPECT_TRUE(std::contiguous_iterator<Array<int32_t>::Iterator>);
+  EXPECT_TRUE(std::contiguous_iterator<Array<int32_t>::ConstIterator>);
+  Array<int32_t> array = {0, 1, 2};
+  for (auto iter = array.begin(); iter != array.end(); ++iter) {  // NOLINT
+    *iter += 1;
+  }
+  for (auto& num : array) {
+    num += 1;
+  }
+  const Array<int32_t> compare = {2, 3, 4};
+  auto arr_iter = array.begin();
+  auto cmp_iter = compare.begin();
+  for (int32_t i = 0; i < 3; ++i) {
+    EXPECT_EQ(arr_iter[i], cmp_iter[i]);
+  }
+}
